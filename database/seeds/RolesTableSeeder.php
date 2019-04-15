e9e9b74b05c72e8b68a9f77d17114a94cb1d0a6b
ernesto <jesh40@hotmail.es>
2019-04-15 11:18:26 -0600

    ajustando roles y pendiente llevar control

--- database/seeds/RolesTableSeeder.php
+++ database/seeds/RolesTableSeeder.php
@@ -12,14 +12,38 @@ class RolesTableSeeder extends Seeder
      */
     public function run()
     {
-        $roles = [
+       /* $roles = [
             'Administrador',
-            'Informatica'
+            'Coordinador',
+            'Cajero',
+            'Vendedor'
         ];
 
 
         foreach ($roles as $rol) {
             Role::create(['name' => $rol]);
-        }
+        }*/
+//DBCC CHECKIDENT('dbo.users', RESEED, 0) en sql ejecutarlo reinicia el autoincremento
+    /*  DB::table('roles')->truncate();*/
+
+        DB::table('roles')->insert(array(
+            array(
+                'nombre' => 'Administrador',
+                'descripcion' => 'Administrador del sistema',
+            ),
+            array(
+                'nombre' => 'Coordinador',
+                'descripcion' => 'Rol coordinador del sistema',
+            ),
+            array(
+                'nombre' => 'Vendedor',
+                'descripcion' => 'Rol vendedor del sistema',
+            ),
+            array(
+                'nombre' => 'Cajero',
+                'descripcion' => 'Rol cajero del sistema',
+            ),
+        ));
+
     }
 }
